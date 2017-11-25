914edaf26e7bb0f40b65ad21ec3ecde019900363
alex <S.Alex.Klein@gmail.com>
2017-11-24 19:14:41 -0700

    Added some different dice distributions and associated file naming

--- craps.py
+++ craps.py
@@ -9,19 +9,33 @@ from dice import Dice
 from Player import Player
 from Table import Table
 
-env = simpy.Environment()
+env = None
+dice_distributions = {
+    'flat': [1, 1, 1, 1, 1, 1],
+    'heavy_3': [1, 1, 2, 1, 1, 1],
+    'very_heavy_3': [1, 1, 3, 1, 1, 1],
+    'heavy_3_light_4': [2, 2, 3, 1, 2, 2],
+    'very_heavy_3_light_4': [2, 2, 4, 1, 2, 2],
+    'heavy_corner': [2, 2, 2, 1, 1, 1],
+    'very_heavy_corner': [3, 3, 3, 1, 1, 1]
+}
 
 
 def run():
-    table = Table(env, Dice())
-    env.process(player_generator(env=env, table=table,
-                                 arrival_dist=dg.Exponential(1/simminutes(10)),
-                                 play_time_dist=dg.Flat(simminutes(30), simhours(3)),
-                                 # play_time_dist=dg.Normal(simhours(1.5), simminutes(45), minimum=30.0),
-                                 wait_time_dist=dg.Flat(simminutes(2), simminutes(15)),
-                                 num_players=100))
-    env.run()
-    plot_rolls(table)
+    global env
+
+    for distr_name, distr in dice_distributions.items():
+        print("Running ", distr_name)
+        env = simpy.Environment()
+        table = Table(env, Dice(distribution=distr))
+        env.process(player_generator(env=env, table=table,
+                                     arrival_dist=dg.Exponential(1/simminutes(10)),
+                                     play_time_dist=dg.Flat(simminutes(30), simhours(3)),
+                                     # play_time_dist=dg.Normal(simhours(1.5), simminutes(45), minimum=30.0),
+                                     wait_time_dist=dg.Flat(simminutes(2), simminutes(15)),
+                                     num_players=100))
+        env.run()
+        plot_rolls(table, distr_name)
 
 
 def player_generator(env, table, arrival_dist, play_time_dist, wait_time_dist, num_players=None):
@@ -40,7 +54,7 @@ def player_generator(env, table, arrival_dist, play_time_dist, wait_time_dist, n
         env.process(player.arrive(env))
 
 
-def plot_rolls(table):
+def plot_rolls(table, file_prefix=None):
     if not os.path.exists('results'):
         os.mkdir('results', 666)
 
@@ -54,8 +68,14 @@ def plot_rolls(table):
     ax1.xaxis.set_ticks(range(1, table.dice.nsides + 1), 1)
     ax2.bar(list(roll_counter.keys()), roll_counter.values())
     ax2.xaxis.set_ticks(range(2, table.dice.nsides * 2 + 1), 1)
-    plt.savefig('results/dice.png', bbox_inches='tight')
-    plt.savefig('results/dice.pdf')
+
+    if file_prefix is None:
+        file_prefix = ''
+    elif file_prefix[-1] != '_':
+        file_prefix = file_prefix + '_'
+
+    plt.savefig('results/{prefix}dice.png'.format(prefix=file_prefix), bbox_inches='tight')
+    plt.savefig('results/{prefix}dice.pdf'.format(prefix=file_prefix))
 
 
 if __name__ == '__main__':
