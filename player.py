a97db017bf2ffa6b907bc4767bec6562dc6dffa1
alex <S.Alex.Klein@gmail.com>
2017-12-03 20:40:27 -0700

    Added buy-in to arrival

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from simtime import pretty_time
+from simtime import seconds, pretty_time
 
 
 class Player(object):
@@ -33,6 +33,9 @@ class Player(object):
             wait = self.t_seated - self.t_arrival
             print(self, " took a spot at the table. Waited ", pretty_time(wait))
             self.table.add_player(self)
+            with self.table.dealer_request(self) as dealer_request, self.table.boxman.request() as boxman_request:
+                yield dealer_request and boxman_request
+                yield env.timeout(seconds(45))
             env.process(self.play(env))
         else:
             spot_request.cancel()
@@ -43,7 +46,7 @@ class Player(object):
     def depart(self, env):
         self.t_departure = env.now
         played = self.t_departure - self.t_seated
-        self.table.spots.release(self.spot)
+        yield self.table.spots.release(self.spot)
         self.table.remove_player(self)
         print(self, " departed at ", pretty_time(env.now), " after playing ", pretty_time(played))
 
@@ -52,7 +55,7 @@ class Player(object):
 
     def play(self, env):
         yield env.timeout(self.play_time)
-        self.depart(env)
+        env.process(self.depart(env))
 
     def place_bet(self, env):
         with self.table.request_dealer(self) as dealer_request:
