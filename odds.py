9ddd81d18c7f2ee588bdee3a097552f4aabac762
alex <S.Alex.Klein@gmail.com>
2017-12-03 20:37:59 -0700

    Fixed fraction and added expected value

--- odds.py
+++ odds.py
@@ -3,7 +3,7 @@ class Odds(object):
     def __init__(self, a, b):
         self.a = a
         self.b = b
-        self.fraction = float(a) / float(b)
+        self.fraction = float(b) / float(a+b)
 
     def __str__(self):
         return "%d:%d odds - %f.02%%".format(self.a, self.b, self.fraction)
@@ -20,13 +20,16 @@ class Odds(object):
     def even_payout(self):
         return Payout(self.b, self.a)
 
+    def expected(self, events):
+        return self.fraction * events
+
 
 class Payout(Odds):
     def __str__(self):
-        return "%d:%d payout"
+        return "%d:%d payout".format(self.a, self.b)
 
     def __call__(self, *args, **kwargs):
         return self.amount(*args)
 
     def amount(self, bet):
-        return bet * self.fraction
+        return bet * 1.0/self.fraction
