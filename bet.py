cedbf80178ee3c0d5a8ba1cfe031a8dd1638fb0a
alex <S.Alex.Klein@gmail.com>
2017-12-03 20:43:58 -0700

    Using the point in the roll event. Added active and action roll counts. Added expected values. Fixed the tall and small bets.

--- bet.py
+++ bet.py
@@ -2,7 +2,6 @@ from odds import *
 
 
 class Bet(object):
-
     def __init__(self, table, name, true_odds, payout, winning_rolls=None, losing_rolls=None):
         self.table = table
         self.name = name
@@ -15,30 +14,39 @@ class Bet(object):
 
         self.hit = 0
         self.miss = 0
+        self.loss = 0
+        self.active_rolls = 0
+        self.action_rolls = 0
         self.hit_intervals = []
         self.since_last_hit = 0
 
-        self.table.env.process(self.handle_roll(self.table.next_roll_event))
+        self.observing = self.table.next_roll_event
+        self.table.env.process(self.handle_roll())
 
-    def handle_roll(self, roll_event):
-        yield roll_event
+    def handle_roll(self):
+        while True:
+            yield self.observing
 
-        if self.table.next_roll_event is not None:
-            self.table.env.process(self.handle_roll(self.table.next_roll_event))
+            roll, point = self.observing.value
+            if roll is None or isinstance(roll, Exception):
+                break
 
-        roll = roll_event.value
-        if roll is None or isinstance(roll, Exception):
-            return
+            if self.table.next_roll_event is not None:
+                self.observing = self.table.next_roll_event
 
-        if not self.is_active():
-            return
+            if not self.is_active(point):
+                continue
 
-        if self.is_hit(roll):
-            self.handle_hit(roll)
-        else:
-            if self.is_loss(roll):
-                self.handle_loss(roll)
-            self.handle_miss(roll)
+            self.active_rolls += 1
+
+            if self.is_hit(roll):
+                self.handle_hit(roll, point)
+                self.action_rolls += 1
+            else:
+                if self.is_loss(roll):
+                    self.handle_loss(roll, point)
+                    self.action_rolls += 1
+                self.handle_miss(roll, point)
 
     def is_hit(self, roll):
         for winner in self.winning_rolls:
@@ -60,28 +68,36 @@ class Bet(object):
 
         return False
 
-    def is_active(self):
+    def is_active(self, point):
         return True
 
-    def handle_hit(self, roll):
+    def handle_hit(self, roll, point):
         self.hit += 1
         self.hit_intervals.append(self.since_last_hit)
         self.since_last_hit = 0
-        self.handle_payout(roll)
+        self.handle_payout(roll, point)
 
-    def handle_payout(self, roll):
+    def handle_payout(self, roll, point):
         self.paid += self.payout.amount(1.0)
 
-    def handle_miss(self, roll):
+    def handle_miss(self, roll, point):
         self.miss += 1
         self.since_last_hit += 1
 
-    def handle_loss(self, roll):
+    def handle_loss(self, roll, point):
         self.raked += 1.0
+
+    def expected(self):
+        return self.true_odds.expected(self.action_rolls)
+
+    def expected_payout(self):
+        return self.expected() * self.payout.amount(1.0)
+
+    def expected_rake(self):
+        return (self.action_rolls - self.expected()) * 1.0
 
 
 class SingleRoll(Bet):
-
     def is_loss(self, roll):
         return not self.is_hit(roll)
 
@@ -98,8 +114,8 @@ class ComeOutBet(Bet):
     A bet that is active before a point is set.
     """
 
-    def is_active(self):
-        return not self.table.has_point()
+    def is_active(self, point):
+        return point is None
 
 
 class PointBet(Bet):
@@ -107,17 +123,17 @@ class PointBet(Bet):
     A bet that is active after a point is set.
     """
 
-    def is_active(self):
-        return self.table.has_point()
+    def is_active(self, point):
+        return point is not None
 
 
 class PlaceBet(PointBet):
     """
     One of [4-10] - [7], can't bet on it when that number is the point
     """
-    
-    def is_active(self):
-        return self.table.point not in self.winning_rolls and super(PlaceBet, self).is_active()
+
+    def is_active(self, point):
+        return point not in self.winning_rolls and super(PlaceBet, self).is_active(point)
 
 
 class FieldBet(PointBet):
@@ -125,7 +141,7 @@ class FieldBet(PointBet):
         super(FieldBet, self).__init__(table, 'Field', Odds(5, 4), Payout(1, 1), [2, 3, 4, 9, 10, 11, 12], [5, 6, 7, 8])
         self.payout_2_12 = Payout(2, 1)
 
-    def handle_payout(self, roll):
+    def handle_payout(self, roll, point):
         if roll in [2, 12]:
             self.paid += self.payout_2_12.amount(1.0)
         else:
@@ -142,8 +158,12 @@ class AllBet(PropBet):
         self.seen[roll.total] = True
         return self.seen == self.required
 
-    def handle_hit(self, roll):
-        super(AllBet, self).handle_hit(roll)
+    def handle_hit(self, roll, point):
+        super(AllBet, self).handle_hit(roll, point)
+        self.seen = [False for _ in range(13)]
+
+    def handle_loss(self, roll, point):
+        super(AllBet, self).handle_loss(roll, point)
         self.seen = [False for _ in range(13)]
 
 
@@ -158,8 +178,12 @@ class TallBet(PropBet):
         self.seen[roll.total - 8] = True
         return all(self.seen)
 
-    def handle_hit(self, roll):
-        super(TallBet, self).handle_hit(roll)
+    def handle_hit(self, roll, point):
+        super(TallBet, self).handle_hit(roll, point)
+        self.seen = [False for _ in range(8, 13)]
+
+    def handle_loss(self, roll, point):
+        super(TallBet, self).handle_loss(roll, point)
         self.seen = [False for _ in range(8, 13)]
 
 
@@ -174,6 +198,10 @@ class SmallBet(PropBet):
         self.seen[roll.total - 2] = True
         return all(self.seen)
 
-    def handle_hit(self, roll):
-        super(SmallBet, self).handle_hit(roll)
+    def handle_hit(self, roll, point):
+        super(SmallBet, self).handle_hit(roll, point)
+        self.seen = [False for _ in range(2, 7)]
+
+    def handle_loss(self, roll, point):
+        super(SmallBet, self).handle_loss(roll, point)
         self.seen = [False for _ in range(2, 7)]
